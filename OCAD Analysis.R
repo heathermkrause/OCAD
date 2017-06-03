f329e783e4c52a41d01e797157d315f1c58637e8
heathermkrause <heather@idatassist.com>
2017-06-03 11:58:16 -0400

    First commit

---
+++ OCAD Analysis.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(lubridate)
+library(ggplot)
+library(nlme)
+library(lattice)
+library(car)
+library(spida2)
+
+data <- read.csv("/Users/heatherkrause/Dropbox/R/OCAD/NCHA Jennifer.csv")
